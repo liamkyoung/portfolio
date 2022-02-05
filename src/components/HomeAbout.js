db7cc55eec7c749c97c7f1438d0cc9fd722e1f2b
Kingston <liamyoung@ufl.edu>
2022-02-05 18:47:33 -0500

    escaped apostrophes

--- src/components/HomeAbout.js
+++ src/components/HomeAbout.js
@@ -10,7 +10,7 @@ function HomeAbout () {
           <Gator height={300} width={300} />
           <div className='max-w-xl font-reem text-xl mx-10 text-center md:text-left'>
             <h1 className='mb-3'>Hello! My name is Kingston Young and I am a Computer Science student at the University of Florida.
-              I'm a self-taught web developer and have been programming for 4 years, and can't wait to start my career as
+              I&apos;m a self-taught web developer and have been programming for 4 years, and can&apos;t wait to start my career as
               a Software Engineer.
             </h1>
             <h1>My current interests involve soccer, skateboarding, programming, and cooking. I am always ready for a challenge and I love to learn new things every day.</h1>
